5d41d1a949796f0275112f29f35e13a704b38f52
daniellyncon <daniellyncon@hotmail.com>
2021-02-23 20:52:43 -0300

    update arquivo de resposta

--- sprint1/mining_script.py
+++ sprint1/mining_script.py
@@ -19,17 +19,17 @@ QUERY = """
         nodes {
           ... on Repository {
             nameWithOwner
-            createdAt
+            createdAt  # RQ 01
             pushedAt
             primaryLanguage {
               name
-            }
+            } 
             stargazers {
               totalCount
             }
             releases {
               totalCount
-            }
+            } # RQ 03
             mergedPRs: pullRequests(states: MERGED) {
               totalCount
             }
@@ -44,24 +44,34 @@ QUERY = """
     }
 }
 """
+# RQ 01 -> ON REPOSITORY/CREATED AT
+# RQ 02 -> pullRequests(merged)/totalCount
+# RQ 03 -> releases/totalCount
+# RQ 04 -> ON REPOSITORY/PUSHED AT
+# RQ 05 -> primaryLanguage / name
+# RQ 06 -> closedIssues / totalCount
 
-response = ''
+condition = True
 
-try:
-    response = requests.post(f'{URL}', json={'query': QUERY}, headers=HEADERS)
-    response.raise_for_status()
-    data = dict(response.json())
-    df = pd.DataFrame(data['data']['search']['nodes'])
-    df.to_csv(os.path.abspath(os.getcwd()) + '/export_dataframe.csv', index=False, header=True)
+while condition:
+    response = ''
 
-except requests.exceptions.ConnectionError:
-    print(f'Connection error during the request')
+    try:
+        response = requests.post(f'{URL}', json={'query': QUERY}, headers=HEADERS)
+        response.raise_for_status()
+        data = dict(response.json())
+        df = pd.DataFrame(data['data']['search']['nodes'])
+        df.to_csv(os.path.abspath(os.getcwd()) + '/export_dataframe.csv', index=False, header=True)
 
-except requests.exceptions.HTTPError:
-    print(f'HTTP request error. STATUS: {response.status_code}')
+    except requests.exceptions.ConnectionError:
+        print(f'Connection error during the request')
 
-except FileNotFoundError:
-    print(f'File not found.')
+    except requests.exceptions.HTTPError:
+        print(f'HTTP request error. STATUS: {response.status_code}')
 
-else:
-    print("Nothing went wrong! Saved ")
+    except FileNotFoundError:
+        print(f'File not found.')
+
+    else:
+        print("Nothing went wrong! Saved csv with mining results")
+        condition = False
