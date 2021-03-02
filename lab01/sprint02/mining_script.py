860e37804bae4afe103851eec516e8a35fdfff84
Carla Vieira <carlaamvieira@gmail.com>
2021-03-02 15:49:38 +0100

    ADD: Paginacao

--- lab01/sprint02/mining_script.py
+++ lab01/sprint02/mining_script.py
@@ -3,6 +3,10 @@ import os
 import requests
 import pandas as pd
 
+num_sprint="02"
+num_nodes_total=50
+num_nodes_request=10
+
 # Retrieves Github API Token from .env
 dotenv.load_dotenv(dotenv.find_dotenv())
 TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
@@ -13,37 +17,77 @@ HEADERS = {
 URL = 'https://api.github.com/graphql'
 
 # Query to be made on the current test
-QUERY = """
- query github {
-    search (query: "stars:>10000", type:REPOSITORY, first:1) {
-        nodes {
-          ... on Repository {
-            nameWithOwner
-            createdAt  # RQ 01
-            pushedAt
-            primaryLanguage {
-              name
-            } 
-            stargazers {
-              totalCount
+def create_query(last_cursor):
+    QUERY = """
+     query github {
+        search (query: "stars:>10000", type:REPOSITORY, first:"""+str(num_nodes_request)+""") {
+            pageInfo {
+                endCursor
+                }
+            nodes {
+              ... on Repository {
+                nameWithOwner
+                createdAt  # RQ 01
+                pushedAt
+                primaryLanguage {
+                  name
+                } 
+                stargazers {
+                  totalCount
+                }
+                releases {
+                  totalCount
+                } # RQ 03
+                mergedPRs: pullRequests(states: MERGED) {
+                  totalCount
+                }
+                closedIssues: issues(states: CLOSED) {
+                  totalCount
+                } 
+                totalIssues: issues {
+                  totalCount
+                }
+              }
             }
-            releases {
-              totalCount
-            } # RQ 03
-            mergedPRs: pullRequests(states: MERGED) {
-              totalCount
-            }
-            closedIssues: issues(states: CLOSED) {
-              totalCount
-            } 
-            totalIssues: issues {
-              totalCount
-            }
-          }
         }
     }
-}
-"""
+    """
+    if last_cursor is not None:
+        QUERY = """
+         query github {
+            search (query: "stars:>10000", type:REPOSITORY, first:"""+str(num_nodes_request)+""", after:"""+"\""+(last_cursor)+"\""+""") {
+                pageInfo {
+                    endCursor
+                    }
+                nodes {
+                  ... on Repository {
+                    nameWithOwner
+                    createdAt  # RQ 01
+                    pushedAt
+                    primaryLanguage {
+                      name
+                    } 
+                    stargazers {
+                      totalCount
+                    }
+                    releases {
+                      totalCount
+                    } # RQ 03
+                    mergedPRs: pullRequests(states: MERGED) {
+                      totalCount
+                    }
+                    closedIssues: issues(states: CLOSED) {
+                      totalCount
+                    } 
+                    totalIssues: issues {
+                      totalCount
+                    }
+                  }
+                }
+            }
+        }
+        """
+    return QUERY
 
 
 # RQ 01 -> ON REPOSITORY/CREATED AT
@@ -53,27 +97,35 @@ QUERY = """
 # RQ 05 -> primaryLanguage / name
 # RQ 06 -> closedIssues / totalCount
 
-condition = True
+last_cursor = None
+nodes = pd.DataFrame()
+pages = num_nodes_total // num_nodes_request
+print(f"It will take {pages} pages")
+for page in range(pages):
 
-while condition:
-    response = ''
+    condition = True
+    while condition:
 
-    try:
-        response = requests.post(f'{URL}', json={'query': QUERY}, headers=HEADERS)
-        response.raise_for_status()
-        data = dict(response.json())
-        df = pd.DataFrame(data['data']['search']['nodes'])
-        df.to_csv(os.path.abspath(os.getcwd()) + '/sprint02/export_dataframe.csv', index=False, header=True)
+        try:
+            response = requests.post(f'{URL}', json={'query': create_query(last_cursor)}, headers=HEADERS)
+            response.raise_for_status()
+            data = dict(response.json())
+            last_cursor=data['data']['search']['pageInfo']['endCursor']
+            df = pd.DataFrame(data['data']['search']['nodes'])
+            nodes = nodes.append(df)
 
-    except requests.exceptions.ConnectionError:
-        print(f'Connection error during the request')
+        except requests.exceptions.ConnectionError:
+            print(f'Connection error during the request')
 
-    except requests.exceptions.HTTPError:
-        print(f'HTTP request error. STATUS: {response.status_code}')
+        except requests.exceptions.HTTPError:
+            print(f'HTTP request error. STATUS: {response.status_code}')
 
-    except FileNotFoundError:
-        print(f'File not found.')
+        except FileNotFoundError:
+            print(f'File not found.')
 
-    else:
-        print("Nothing went wrong! Saved csv with mining results")
-        condition = False
+        else:
+            print(f"Page {page} succeeded!")
+            condition = False
+
+nodes.to_csv(os.path.abspath(os.getcwd()) + f'/sprint{num_sprint}/export_dataframe.csv', index=False, header=True)
+print("Successful mining! Saved csv with mining results")
