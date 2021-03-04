e558e3247fcd267358a4f3e953bcb574315b41f7
daniellyncon <daniellyncon@hotmail.com>
2021-03-03 21:41:11 -0300

    code refactor and columns update

--- lab01/sprint02/mining_script.py
+++ lab01/sprint02/mining_script.py
@@ -1,11 +1,12 @@
-import dotenv
 import os
+import dotenv
 import requests
 import pandas as pd
+from datetime import date, datetime
 
-num_sprint="02"
-num_nodes_total=1000
-num_nodes_request=5
+num_sprint = "02"
+num_nodes_total = 200
+num_nodes_request = 5
 
 # Retrieves Github API Token from .env
 dotenv.load_dotenv(dotenv.find_dotenv())
@@ -16,11 +17,12 @@ HEADERS = {
 }
 URL = 'https://api.github.com/graphql'
 
+
 # Query to be made on the current test
-def create_query(last_cursor):
-    QUERY = """
+def create_query(cursor):
+    query = """
      query github {
-        search (query: "stars:>10000", type:REPOSITORY, first:"""+str(num_nodes_request)+""") {
+        search (query: "stars:>10000", type:REPOSITORY, first:""" + str(num_nodes_request) + """) {
             pageInfo {
                 endCursor
                 }
@@ -52,17 +54,18 @@ def create_query(last_cursor):
         }
     }
     """
-    if last_cursor is not None:
-        QUERY = """
+    if cursor is not None:
+        query = """
          query github {
-            search (query: "stars:>10000", type:REPOSITORY, first:"""+str(num_nodes_request)+""", after:"""+"\""+(last_cursor)+"\""+""") {
+            search (query: "stars:>10000", type:REPOSITORY, first:""" + str(
+            num_nodes_request) + """, after:""" + "\"" + cursor + "\"" + """) {
                 pageInfo {
                     endCursor
                     }
                 nodes {
                   ... on Repository {
                     nameWithOwner
-                    createdAt  # RQ 01
+                    createdAt
                     pushedAt
                     primaryLanguage {
                       name
@@ -87,20 +90,27 @@ def create_query(last_cursor):
             }
         }
         """
-    return QUERY
+    return query
 
 
-# RQ 01 -> ON REPOSITORY/CREATED AT
-# RQ 02 -> pullRequests(merged)/totalCount
-# RQ 03 -> releases/totalCount
-# RQ 04 -> ON REPOSITORY/PUSHED AT
-# RQ 05 -> primaryLanguage / name
-# RQ 06 -> closedIssues / totalCount
+def calculate_age(date_time_string):
+    today = datetime.today()
+    date_time_obj = datetime.strptime(date_time_string[0:10], "%Y-%m-%d")
+    return (today - date_time_obj).days
+
+
+def calculate_closed_issues_percent(op1, op2):
+    if op2 > 0:
+        return (op1/op2)*100
+
+    return None
+
 
 last_cursor = None
 nodes = pd.DataFrame()
 pages = num_nodes_total // num_nodes_request
 print(f"It will take {pages} pages")
+response = ""
 for page in range(pages):
 
     condition = True
@@ -110,7 +120,8 @@ for page in range(pages):
             response = requests.post(f'{URL}', json={'query': create_query(last_cursor)}, headers=HEADERS)
             response.raise_for_status()
             data = dict(response.json())
-            last_cursor=data['data']['search']['pageInfo']['endCursor']
+            last_cursor = data['data']['search']['pageInfo']['endCursor']
+
             df = pd.DataFrame(data['data']['search']['nodes'])
             nodes = nodes.append(df)
 
@@ -124,8 +135,29 @@ for page in range(pages):
             print(f'File not found.')
 
         else:
-            print(f"Page {page}/{pages} succeeded!")
+            print(f"Page {page+1}/{pages} succeeded!")
             condition = False
+
+nodes['Repository Age'] = ''
+nodes['Accepted Pull Requests'] = ''
+nodes['Total Releases'] = ''
+nodes['Time Since Last Update'] = ''
+nodes['Language'] = ''
+nodes['Closed Issues Percent'] = ''
+
+for index, row in nodes.iterrows():
+
+    nodes.loc[index, 'Repository Age'] = calculate_age(row['createdAt'])
+    nodes.loc[index, 'Accepted Pull Requests'] = row['mergedPRs']['totalCount']
+    nodes.loc[index, 'Total Releases'] = row['releases']['totalCount']
+    nodes.loc[index, 'Days Since Last Update'] = calculate_age(row['pushedAt'])
+    nodes.loc[index, 'Closed Issues Percent'] = calculate_closed_issues_percent(row['closedIssues']['totalCount'],
+                                                                                row['totalIssues']['totalCount'])
+    if row['primaryLanguage']:
+        nodes.loc[index, 'Language'] = row['primaryLanguage']['name']
+    else:
+        nodes.loc[index, 'Language'] = None
+
 
 nodes.to_csv(os.path.abspath(os.getcwd()) + f'/sprint{num_sprint}/export_dataframe.csv', index=False, header=True)
 print("Successful mining! Saved csv with mining results")
