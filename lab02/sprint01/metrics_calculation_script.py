13915f643c4c1668f344058a15ba46c228f1fd54
daniellyncon <daniellyncon@hotmail.com>
2021-03-18 07:46:24 -0300

    correcao tratamento de excecao

--- lab02/sprint01/metrics_calculation_script.py
+++ lab02/sprint01/metrics_calculation_script.py
@@ -3,7 +3,6 @@ import dotenv
 import pandas as pd
 from github import Github
 
-
 dotenv.load_dotenv(dotenv.find_dotenv())
 TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
 
@@ -14,14 +13,13 @@ github_user = github.get_user()
 
 nodes = pd.read_csv(os.path.abspath(os.getcwd()) + "/export_dataframe.csv")
 
-
 for index, row in nodes.iterrows():
-    if (str(row['CBO'])=="nan" or str(row['DIT'])=="nan" or str(row['WMC'])=="nan" or str(row['LOC'])=="nan"):
+    if str(row['CBO']) == "nan" or str(row['DIT']) == "nan" or str(row['WMC']) == "nan" or str(row['LOC']) == "nan":
         try:
             original_repo = github.get_repo(row['Owner/Repository'])
             cmd = "git clone {}".format(original_repo.clone_url)
             print("\n\n #####################################")
-            print("Starting to clone {}. The {}th repo...".format(original_repo.name, index+1))
+            print("Starting to clone {}. The {}th repo...".format(original_repo.name, index + 1))
             print("Running command '{}'".format(cmd))
 
             os.system(cmd)
@@ -34,6 +32,7 @@ for index, row in nodes.iterrows():
             metrics_df = pd.read_csv(os.path.abspath(os.getcwd()) + "/class.csv", usecols=['cbo', 'dit', 'wmc', 'loc'])
             # Cálculo das medianas de cada métrica do repositório atual
             medians = metrics_df.median(skipna=True)
+            soma = metrics_df.sum(skipna=True)
             """
             Se definirmos skipna=True, ele ignora a NaN no campo de dados.
             Isto nos permite calcular a mediana do DataFrame ao longo do eixo da coluna, ignorando os valores NaN.
@@ -42,11 +41,11 @@ for index, row in nodes.iterrows():
             nodes.loc[index, 'CBO'] = medians['cbo']
             nodes.loc[index, 'DIT'] = medians['dit']
             nodes.loc[index, 'WMC'] = medians['wmc']
-            nodes.loc[index, 'LOC'] = medians['loc']
+            nodes.loc[index, 'LOC'] = soma['loc']
 
-            print(f"Saving metrics for {original_repo.name}: CBO={medians['cbo']}, DIT={medians['dit']}, WMC={medians['wmc']}, LOC={medians['loc']}.")
+            print(f"Saving metrics for {original_repo.name}: CBO={medians['cbo']}, DIT={medians['dit']}, "
+                  f"WMC={medians['wmc']}, LOC={soma['loc']}.")
             nodes.to_csv(os.path.abspath(os.getcwd()) + f'/export_dataframe.csv', index=False, header=True)
-
 
             # Comando para apagar o repositório clonado a cada iteração
             cmd = "rm -rf {}".format(original_repo.name)
@@ -58,13 +57,9 @@ for index, row in nodes.iterrows():
                 raise Exception("Error when apply metrics...")
 
         except Exception as e:
-            cmd = "rm -rf {}".format(original_repo.name)
-            os.system(cmd)
-            print("Finished removing {}".format(original_repo.name))
-            print('Failure at repo {}'.format(original_repo.name))
+            print('Failure at repo {}'.format(index))
             print("#####################################")
             print(e)
 
-
-nodes.to_csv(os.path.abspath(os.getcwd()) + f'/dataframe_with_metrics.csv', index=False, header=True)
+# nodes.to_csv(os.path.abspath(os.getcwd()) + f'/dataframe_with_metrics.csv', index=False, header=True)
 print("Metrics Calculated Successfully! Saved csv with mining results")
