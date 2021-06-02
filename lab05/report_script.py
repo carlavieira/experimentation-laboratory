f75a6db965a76d0995bcb764c004a990b354aafe
Carla d'Abreu <carlaamvieira@gmail.com>
2021-06-02 17:44:37 +0200

    ADD: results

--- lab05/report_script.py
+++ lab05/report_script.py
@@ -6,48 +6,56 @@ import time
 import dotenv
 
 NUM_ITERATION = 5
+NUM_PER_PAGE = 10
+NUM_PAGES = 10
 
 dotenv.load_dotenv()
 
-URL = 'https://api.github.com/graphql'
+URL_GRAPHQL = 'https://api.github.com/graphql'
+URL_REST = f'https://api.github.com/search/users?q=location%3AUS&per_page={NUM_PER_PAGE}&page='
 TOKEN_LIST = json.loads(os.getenv("GITHUB_ACCESS_TOKENS"))
 
 
 def generate_new_header():
+    global token_index
     new_header = {
         'Content-Type': 'application/json',
-        'Authorization': f'bearer {TOKEN_LIST[0]}'
+        'Authorization': f'bearer {TOKEN_LIST[token_index]}'
     }
+    if token_index < len(TOKEN_LIST) - 1:
+        token_index += 1
+    else:
+        token_index = 0
     return new_header
 
 
-def create_query(cursor=None, complete=False):
-    if cursor is None:
-        cursor = 'null'
+def create_query_graphql(cursor=None, all_attributes=False, with_pagination=False):
+    if with_pagination:
+        if cursor is None:
+            cursor = 'null'
 
-    else:
-        cursor = '\"{}\"'.format(cursor)
-    query = """
-    query github {
-        search (query: "location:US",  type: USER, first: 5, after: %s) {
-            edges {
-              node {
-                ... on User {
-                  id
+        else:
+            cursor = '\"{}\"'.format(cursor)
+
+    if not with_pagination and not all_attributes:
+        query = """
+        query github {
+            search (query: "location:US",  type: USER, first: %s) {
+                edges {
+                  node {
+                    ... on User {
+                      id
+                    }
+                  }
                 }
               }
             }
-            pageInfo {
-              endCursor
-            }
-          }
-        }
-    """ % (cursor)
+        """ % (NUM_PER_PAGE)
 
-    if complete:
+    elif with_pagination and not all_attributes:
         query = """
         query github {
-            search (query: "location:US",  type: USER, first: 5, after: %s) {
+            search (query: "location:US",  type: USER, first: %s, after: %s) {
                 edges {
                   node {
                     ... on User {
@@ -60,61 +68,153 @@ def create_query(cursor=None, complete=False):
                 }
               }
             }
-        """ % (cursor)
+        """ % (NUM_PER_PAGE, cursor)
+
+    elif not with_pagination and all_attributes:
+        query = """
+         query github {
+             search (query: "location:US",  type: USER, first: %s) {
+                 userCount
+                 edges {
+                   node {
+                     ... on User {
+                       login
+                       databaseId
+                       id
+                       avatarUrl
+                       url
+                       isSiteAdmin
+                     }
+                   }
+                 }
+               }
+             }
+         """ % (NUM_PER_PAGE)
+
+    elif with_pagination and all_attributes:
+        query = """
+         query github {
+             search (query: "location:US",  type: USER, first: %s, after: %s) {
+                 userCount
+                 edges {
+                   node {
+                     ... on User {
+                       login
+                       databaseId
+                       id
+                       avatarUrl
+                       url
+                       isSiteAdmin
+                     }
+                   }
+                 }
+                pageInfo {
+                  endCursor
+                }
+              }
+            }
+        """ % (NUM_PER_PAGE, cursor)
+
     return query
 
 
-if __name__ == "__main__":
-    print(f"\n**** Starting APIs Reports *****\n")
-    print(f"\n**** Starting GraphQL *****\n")
-    headers = generate_new_header()
-    print(f"\n**** One attribute *****\n")
-    print(f"\n**** No pagination *****\n")
-    iteration = 1
-    last_cursor = None
-    while iteration <= NUM_ITERATION:
-        try:
-            start = time.time()
-            response = requests.post(f'{URL}', json={'query': create_query(last_cursor)}, headers=headers)
-            end = time.time()
-            size = len(str(response.json()))
-
-        except requests.exceptions.ConnectionError:
-            print(f'Connection error during the request')
-
-        except requests.exceptions.HTTPError:
-            print(f'HTTP request error. STATUS: {response.status_code}')
-
-        except requests.exceptions.ChunkedEncodingError:
-            print('A bizarre error occurred...')
-
+def request_graphql(header, all_attributes=False, with_pagination=False):
+    try:
+        if not with_pagination:
+            response = requests.post(f'{URL_GRAPHQL}',
+                                     json={'query': create_query_graphql(None, all_attributes, with_pagination)},
+                                     headers=header)
+            response.raise_for_status()
+            response_size = len(str(response.json()))
         else:
-            print(f"[GRAPHQL; ONE ATTRIBUTE; NO PAGINATION] Iteration {iteration}\nTime:{end - start}\nSize:{size}\n")
-            iteration+=1
-
-    print(f"\n**** With pagination *****\n")
-    iteration = 1
-    while iteration <= NUM_ITERATION:
-        try:
-            size = 0
+            response_size = 0
             last_cursor = None
-            start = time.time()
-            for _ in range(100):
-                response = requests.post(f'{URL}', json={'query': create_query(last_cursor)}, headers=headers)
+            for _ in range(NUM_PAGES):
+                response = requests.post(f'{URL_GRAPHQL}', json={
+                    'query': create_query_graphql(last_cursor, all_attributes, with_pagination)}, headers=header)
                 response.raise_for_status()
                 last_cursor = dict(response.json())['data']['search']['pageInfo']['endCursor']
-                size += len(str(response.json()))
-            end = time.time()
+                response_size += len(str(response.json()))
+    except requests.exceptions.ConnectionError:
+        print(f'Connection error during the request')
 
-        except requests.exceptions.ConnectionError:
-            print(f'Connection error during the request')
+    except requests.exceptions.HTTPError:
+        print(f'HTTP request error. STATUS: {response.status_code}')
 
-        except requests.exceptions.HTTPError:
-            print(f'HTTP request error. STATUS: {response.status_code}')
+    except requests.exceptions.ChunkedEncodingError:
+        print('A bizarre error occurred...')
 
-        except requests.exceptions.ChunkedEncodingError:
-            print('A bizarre error occurred...')
+    else:
+        return response_size
 
+
+def request_rest(header, with_pagination=False):
+    try:
+        if not with_pagination:
+            url = URL_REST + '1'
+            response = requests.get(url, headers=header)
+            response.raise_for_status()
+            response_size = len(str(response.json()))
         else:
-            print(f"[GRAPHQL; ONE ATTRIBUTE; WITH PAGINATION]Iteration {iteration}\nTime:{end - start}\nSize:{size}\n")
-            iteration += 1
+            response_size = 0
+            for page in range(1, NUM_PAGES + 1):
+                url = URL_REST + str(page)
+                condition = True
+                while condition:
+                    try:
+                        response = requests.get(url, headers=header)
+                        response.raise_for_status()
+                    except requests.exceptions.HTTPError:
+                        print(f'HTTP request error. STATUS: {response.status_code}')
+                        print(response.json())
+                        header = generate_new_header()
+                    else:
+                        response_size += len(str(response.json()))
+                        condition = False
+
+    except requests.exceptions.ConnectionError:
+        print(f'Connection error during the request')
+
+    except requests.exceptions.HTTPError:
+        print(f'HTTP request error. STATUS: {response.status_code}')
+
+    except requests.exceptions.ChunkedEncodingError:
+        print('A bizarre error occurred...')
+
+    else:
+        return response_size
+
+
+if __name__ == "__main__":
+    global header
+    token_index = 1
+    header = generate_new_header()
+    print(f"\n**** Starting APIs Reports *****\n")
+    print(f'NUM_ITERATION = {NUM_ITERATION}\nNUM_PER_PAGE = {NUM_PER_PAGE}\nNUM_PAGES = {NUM_PAGES}')
+    print(f"\n**** Starting GraphQL *****\n")
+    for with_pagination in [False, True]:
+        for all_attributes in [False, True]:
+            iteration = 1
+            last_cursor = None
+            while iteration <= NUM_ITERATION:
+                start = time.time()
+                size = request_graphql(header, all_attributes, with_pagination)
+                end = time.time()
+
+                print(
+                    f"[GRAPHQL;{'ALL ATTRIBUTES' if all_attributes else 'ONE ATTRIBUTE'}; {'WITH PAGINATION' if with_pagination else 'ONE PAGE'}] Iteration {iteration}\nTime:{end - start}\nSize:{size}\n")
+                iteration += 1
+
+    print(f"\n**** Starting REST *****\n")
+    for with_pagination in [False, True]:
+        for all_attributes in [False, True]:
+            iteration = 1
+            last_cursor = None
+            while iteration <= NUM_ITERATION:
+                start = time.time()
+                size = request_rest(header, with_pagination)
+                end = time.time()
+
+                print(
+                    f"[REST;{'ALL ATTRIBUTES' if all_attributes else 'ONE ATTRIBUTE'}; {'WITH PAGINATION' if with_pagination else 'ONE PAGE'}] Iteration {iteration}\nTime:{end - start}\nSize:{size}\n")
+                iteration += 1
